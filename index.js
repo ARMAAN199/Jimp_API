a28b5c4c9f8a2e6802b8a83c8c2337441645d190
ARMAAN199 <56539435+ARMAAN199@users.noreply.github.com>
2022-02-16 01:38:00 +0530

    jimp

--- index.js
+++ index.js
@@ -10,13 +10,22 @@ console.log("Hello");
 //       .write('lena-small-bw.jpg'); // save
 //   });
 
-Jimp.read("./img.png")
+Jimp.read("./imgs.png")
   .then((image) => {
-    return image
-      .resize(256, 256) // resize
-      .quality(60) // set JPEG quality
-      .greyscale() // set greyscale
-      .write("lena-small-bw.jpg");
+    Jimp.loadFont(Jimp.FONT_SANS_32_BLACK).then((font) => {
+      image
+        .print(
+          font,
+          10,
+          10,
+          "Hello world that wraps!",
+          50,
+          (err, image, { x, y }) => {
+            image.print(font, x, y + 20, "More text on another line", 50);
+          }
+        )
+        .write("output.jpg");
+    });
   })
   .catch((err) => {
     console.log("Image could not be read (sad face emoji)");
