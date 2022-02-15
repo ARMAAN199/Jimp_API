b715bc369dbffee997fd22f699895f8af8ce63cb
ARMAAN199 <56539435+ARMAAN199@users.noreply.github.com>
2022-02-15 16:00:09 +0530

    jimp

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 var Jimp = require("jimp");
 console.log("Hello");
+
+// Jimp.read('lenna.png', (err, lenna) => {
+//     if (err) throw err;
+//     lenna
+//       .resize(256, 256) // resize
+//       .quality(60) // set JPEG quality
+//       .greyscale() // set greyscale
+//       .write('lena-small-bw.jpg'); // save
+//   });
