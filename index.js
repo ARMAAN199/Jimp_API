400742f484b4882ac36ea01ee944d9cc3b82988e
ARMAAN199 <56539435+ARMAAN199@users.noreply.github.com>
2022-02-16 14:36:42 +0530

    d3

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var Jimp = require("jimp");
-var uuid = require("uuid");
-console.log("Hello");
+// var Jimp = require("jimp");
+// var uuid = require("uuid");
+// console.log("Hello");
 
 // Jimp.read('lenna.png', (err, lenna) => {
 //     if (err) throw err;
@@ -11,15 +11,15 @@ console.log("Hello");
 //       .write('lena-small-bw.jpg'); // save
 //   });
 
-Jimp.read("./imgs.png")
-  .then((image) => {
-    var imgname = uuid.v4();
-    Jimp.loadFont(Jimp.FONT_SANS_32_BLACK).then((font) => {
-      image
-        .print(font, 10, 10, "Hello world that wraps!")
-        .write(`${imgname}.png`);
-    });
-  })
-  .catch((err) => {
-    console.log("Image could not be read (sad face emoji)");
-  });
+// Jimp.read("./imgs.png")
+//   .then((image) => {
+//     var imgname = uuid.v4();
+//     Jimp.loadFont(Jimp.FONT_SANS_32_BLACK).then((font) => {
+//       image
+//         .print(font, 10, 10, "Hello world that wraps!")
+//         .write(`${imgname}.png`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.log("Image could not be read (sad face emoji)");
+//   });
