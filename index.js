6071b480d42284651520b2905276c0aa2ed443aa
ARMAAN199 <56539435+ARMAAN199@users.noreply.github.com>
2022-02-16 14:59:28 +0530

    d3

--- index.js
+++ index.js
@@ -2,15 +2,6 @@
 // var uuid = require("uuid");
 // console.log("Hello");
 
-// Jimp.read('lenna.png', (err, lenna) => {
-//     if (err) throw err;
-//     lenna
-//       .resize(256, 256) // resize
-//       .quality(60) // set JPEG quality
-//       .greyscale() // set greyscale
-//       .write('lena-small-bw.jpg'); // save
-//   });
-
 // Jimp.read("./imgs.png")
 //   .then((image) => {
 //     var imgname = uuid.v4();
@@ -23,3 +14,27 @@
 //   .catch((err) => {
 //     console.log("Image could not be read (sad face emoji)");
 //   });
+
+var jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+var d3 = require("d3");
+const fs = require("fs");
+
+const dom = new JSDOM(`<!DOCTYPE html><body></body>`);
+
+let body = d3.select(dom.window.document.querySelector("body"));
+let svg = body
+  .append("svg")
+  .attr("width", 100)
+  .attr("height", 100)
+  .attr("xmlns", "http://www.w3.org/2000/svg");
+svg
+  .append("rect")
+  .attr("x", 10)
+  .attr("y", 10)
+  .attr("width", 80)
+  .attr("height", 80)
+  .style("fill", "orange");
+
+fs.writeFileSync("out.svg", body.html());
